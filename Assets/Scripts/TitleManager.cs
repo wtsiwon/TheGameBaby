b03cb638cc7a8d580e5153878917fc5d3aa23ebb
wtsiwon <siwon4727@gmail.com>
2022-05-31 16:51:47 +0900

    저장

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TitleManager : MonoBehaviour
+public class TitleManager : Singleton<TitleManager>
 {
     [Header("TitleMenu")]
     [SerializeField]
     private Image Title;
+
+    
 }
