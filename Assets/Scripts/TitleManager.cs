d4d445da4be85996b83e4f1415ffc5da6008dc0a
wtsiwon <siwon4727@gmail.com>
2022-05-31 09:53:31 +0900

    resource추가+Dontdestroy추가+singleton추가+BtnManager추가

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -2,10 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
-public class TitleManager : MonoBehaviour
+public class TitleManager : Singleton<TitleManager>
 {
     [Header("TitleMenu")]
     [SerializeField]
-    private Image Title;
+    private Image title;
+    [SerializeField]
+    private Button gameStart;
+    
+    public bool selectStage;
+    
+
+    private void Start()
+    {
+        
+    }
+    public void GoSelectStage()
+    {
+        
+    }
+    private void titleMove()
+    {
+        title.transform.DOMoveY(1000, 1.5f).SetEase(Ease.InBack);
+    }
+
 }
