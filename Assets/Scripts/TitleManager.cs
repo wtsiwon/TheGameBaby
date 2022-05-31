3952d6ddde6793bc16200d5513e636a90558a69b
wtsiwon <siwon4727@gmail.com>
2022-05-31 20:13:18 +0900

    이름...바꿧다...

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -10,10 +10,10 @@ public class TitleManager : Singleton<TitleManager>
     [SerializeField]
     private Image title;
     [SerializeField]
-    private Button gameStart;
+    private Button backGroundButton;
 
     private Tween titleTween;
-    private Tween startTween;
+    
 
     public bool selectStage;
     private bool isStart = true;
@@ -22,10 +22,11 @@ public class TitleManager : Singleton<TitleManager>
     {
         MoveTitle();
         GoSelectStage();
+        SetResolution();
     }
     private void GoSelectStage()
     {
-        gameStart.onClick.AddListener(() =>
+        backGroundButton.onClick.AddListener(() =>
         {
             TitleMove();
         });
@@ -37,9 +38,31 @@ public class TitleManager : Singleton<TitleManager>
     private void TitleMove()
     {
         titleTween.Pause();
+
         title.transform.DOLocalMoveY(1000, 1.5f).SetEase(Ease.InBack);
-        gameStart.transform.DOLocalMoveY(-1000, 1.5f).SetEase(Ease.InBack);
+
+        backGroundButton.transform.DOLocalMoveY(-1000, 1.5f).SetEase(Ease.InBack);
         isStart = false;
     }
+    public void SetResolution()
+    {
+        int setWidth = 1920; // 사용자 설정 너비
+        int setHeight = 1080; // 사용자 설정 높이
 
+        int deviceWidth = Screen.width; // 기기 너비 저장
+        int deviceHeight = Screen.height; // 기기 높이 저장
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
+    }
 }
