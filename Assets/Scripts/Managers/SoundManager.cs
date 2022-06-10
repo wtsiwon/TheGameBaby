272377ce5ba5ee761394fe7d973bb171ef26ed22
wtsiwon <siwon4727@gmail.com>
2022-06-10 11:05:08 +0900

    사운드 메니저 List추가

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -6,20 +6,84 @@ public enum ESoundType
 {
     BGM,
     SFX,
-    Narration
+    first,
+    second,
+    third,
+    fourth,
+    fifth
 }
 public class SoundManager : SingletonDDOL<SoundManager>
 {
     [SerializeField]
-    public List<AudioSource> audioSources = new List<AudioSource>();
+    private List<AudioClip> firstaudio = new List<AudioClip>();
+    [SerializeField]
+    private List<AudioClip> secondaudio = new List<AudioClip>();
+    [SerializeField]
+    private List<AudioClip> thirdaudio = new List<AudioClip>();
+    [SerializeField]
+    private List<AudioClip> fourthaudio = new List<AudioClip>();
+    [SerializeField]
+    private List<AudioClip> fifthaudio = new List<AudioClip>();
+    [SerializeField]
+    private List<AudioClip> BGM = new List<AudioClip>();
+    [SerializeField]
+    private List<AudioClip> SFX = new List<AudioClip>();
 
-    private Dictionary<ESoundType, AudioClip> sounds = new Dictionary<ESoundType, AudioClip>();
+
+    private Dictionary<ESoundType, List<AudioClip>> sounds = new Dictionary<ESoundType, List<AudioClip>>();
 
     private void Start()
     {
-        foreach (var audio in audioSources)
+        foreach(var sound in BGM)
         {
-            //Dictionary에 List에 있는 사운드 삽입
+            sounds[ESoundType.BGM].Add(sound);
+        }
+        foreach(var sound in SFX)
+        {
+            sounds[ESoundType.SFX].Add(sound);
+        }
+    }
+    /// <summary>
+    /// 나레이션 종류받아서 정하는 함수
+    /// </summary>
+    /// <param name="sound">열거형으로 나레이션 종류를 정함</param>
+
+    public void SelectNarration(ESoundType sound)
+    {
+        sounds[sound] = null;
+
+        switch(sound)
+        {
+            case ESoundType.first:
+                foreach(var audio in firstaudio)
+                {
+                    sounds[sound].Add(audio);
+                }
+                break;
+            case ESoundType.second:
+                foreach (var audio in secondaudio)
+                {
+                    sounds[sound].Add(audio);
+                }
+                break;
+            case ESoundType.third:
+                foreach (var audio in thirdaudio)
+                {
+                    sounds[sound].Add(audio);
+                }
+                break;
+            case ESoundType.fourth:
+                foreach (var audio in fourthaudio)
+                {
+                    sounds[sound].Add(audio);
+                }
+                break;
+            case ESoundType.fifth:
+                foreach (var audio in fifthaudio)
+                {
+                    sounds[sound].Add(audio);
+                }
+                break;
         }
     }
 }
