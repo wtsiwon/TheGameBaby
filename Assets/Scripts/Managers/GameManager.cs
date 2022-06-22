4b0ebdded4e25e27145037075be66ca756a03c68
wtsiwon <siwon4727@gmail.com>
2022-06-22 20:49:10 +0900

    제발 이미지 바꾸는거 만들어

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : SingletonDDOL<GameManager>
 {
@@ -9,7 +10,27 @@ public class GameManager : SingletonDDOL<GameManager>
     public bool isUp;
     public bool selectGuide;
 
+    public Image[] slots;
+    public Image wordimg;
+
     [HideInInspector]
     public GameObject guideAnimal;
+
+    [SerializeField]
+    private StageScene stage;
+
+    public void StageFnc()
+    {
+        switch (Stage)
+        {
+            case 1:
+                for (int i = 0; i < 3; i++)
+                {
+                    slots[i].sprite = stage.stages[1].slot1;
+                }
+                
+                break;
+        }
+    }
 
 }
--- Assets/Scripts/Managers/StageScene.cs
+++ Assets/Scripts/Managers/StageScene.cs
@@ -6,17 +6,18 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "SceneNum", menuName = "Scenes/SceneData", order = 1)]
 public class StageScene : ScriptableObject
 {
-    public Stage[] stage;
+    public Stage[] stages;
 }
 
 [System.Serializable]
 public class Stage
 {
-    public Image slot1;
-    public Image slot2;
-    public Image slot3;
-    
-    public Image word;
-    public Image back;
-    public Image ground;
+    public Sprite slot1;
+    public Sprite slot2;
+    public Sprite slot3;
+           
+    public Sprite word;//맞추기 전 글자
+    public Sprite coword;//맞추고 나오는 글자
+    public Sprite back;
+    public Sprite ground;
 }
