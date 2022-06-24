b5b43f2857eb4cc6f46aa0a9538c09a960808c8a
wtsiwon <siwon4727@gmail.com>
2022-06-24 20:52:52 +0900

    홀리몰리

--- Assets/Scripts/Managers/StageData.cs
+++ Assets/Scripts/Managers/StageData.cs
@@ -15,13 +15,158 @@ public class StageData : SingletonDDOL<StageData>
     private Image coWords;
     [SerializeField]
     private Image ground;
-    
+    [SerializeField]
+
+
     /// <summary>
     ///스테이지에 맞는 사진으로 바꿔주는 함수
     /// </summary>
     /// <param name="stagenum">Stage</param>
-    public void LoadStage(int stageNum)
+    public void LoadStage(int stageNum, int step)
     {
-        slots[0].sprite = stagedata.stages[stageNum - 1].steps[0].slot1;
+        switch (stageNum)
+        {
+            case 0:
+                switch (step)
+                {
+                    case 0:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 1:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 2:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                }
+                break;
+            case 1:
+                switch (step)
+                {
+                    case 0:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 1:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 2:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 3:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 4:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                }
+                break;
+            case 2:
+                switch (step)
+                {
+                    case 0:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 1:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 2:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 3:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 4:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                }
+                break;
+            case 3:
+                switch (step)
+                {
+                    case 0:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 1:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 2:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 3:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 4:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                }
+                break;
+            case 4:
+                switch (step)
+                {
+                    case 0:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 1:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 2:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 3:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                    case 4:
+                        slots[0].sprite = stagedata.stages[stageNum].steps[step].slot1;
+                        slots[1].sprite = stagedata.stages[stageNum].steps[step].slot2;
+                        slots[2].sprite = stagedata.stages[stageNum].steps[step].slot3;
+                        break;
+                }
+                break;
+
+        }
     }
 }
